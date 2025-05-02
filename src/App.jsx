f4d3fcb1f63857b0002f34a43a5e1ca48cdb306d
galeed gutierrez <galeedeveloper@gmail.com>
2025-05-02 19:47:26 -0300

    refactor: clean up imports and enhance TaskList component documentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-/* eslint-disable unused-imports/no-unused-vars */
-/* eslint-disable no-unused-vars */
 import { TaskInput } from '@components/TaskInput';
+import { TaskList } from '@components/TaskList';
 import { useState } from 'react';
-// import { TaskList } from '@components/TaskList';
-// Ensure the file exists or update the path if necessary
-/** @import { Task } from './models/Task.model.js' */
+/** @import { Task } from '@models/Task.model.js' */
 
 /**
  * @description Main App component
@@ -12,17 +9,16 @@ import { useState } from 'react';
  * @example <App />
  */
 function App() {
-	// @ts-ignore
 	const [tasks, setTasks] = useState(/** @type {Task[]} */ ([]));
 
 	return (
 		<>
 			<h1>Lista de tareas</h1>
 			<TaskInput setTasks={setTasks} />
-			{/* <TaskList
+			<TaskList
 				setTasks={setTasks}
 				tasks={tasks}
-			/>  */}
+			/>
 		</>
 	);
 }
--- src/components/TaskList/index.jsx
+++ src/components/TaskList/index.jsx
@@ -2,7 +2,12 @@ import { TaskItem } from '../TaskItem';
 /** @import { Task } from '@models/Task.model' */
 
 /**
- * @param {{ tasks: Task[], setTasks: React.Dispatch<React.SetStateAction<Task[]>> }} props
+ * @description This component is used to display the list of tasks and allows the user to interact with them.
+ * @param {object} props - The component props
+ * @param {Task[]} props.tasks - The tasks array.
+ * @param {React.Dispatch<React.SetStateAction<Task[]>>} props.setTasks - The function to set the tasks.
+ * @returns {import('react').JSX.Element}
+ * @example <TaskList task={tasks} setTasks={setTasks}/>
  */
 export const TaskList = ({ tasks, setTasks }) => {
 	if (tasks.length === 0) {
